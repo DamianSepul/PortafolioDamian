21c2a578074a9cfffa788a2f620604f0a10e295b
Damián Sepulveda <damiansepul98@gmail.com>
2022-07-17 22:17:15 -0700

    make a background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import SideBar from "./components/sidebar/SideBar";
 import "./stylesheets/App.css"
-
+import Home from "./components/page-content/Home";
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 function App() {
   return (
     <div className="App">
-      
-      <SideBar />
-
+      <Container  fluid id="ContainerHome">
+        <Row>
+          <Col  >
+            <SideBar />
+          </Col>
+          <Col xs={10}>
+            <Home />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/page-content/Home.jsx
@@ -0,0 +1,14 @@
+import "../../stylesheets/HomePage.css"
+const Home = () => {
+    return ( 
+        <div>
+            <div className="HomePageImage">
+                <h1>
+                    Hello World !
+                </h1>
+            </div>
+        </div>
+     );
+}
+ 
+export default Home;
